20c1a6d031475791d4f412f18ac024af58bd95ef
Shane Rice <shanerice17@gmail.com>
2025-02-20 07:58:06 -0300

    adjust deployment to allow for new clarity env var

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true, // Disables Next.js Image Optimization (needed for static export)
   },
+  env: {
+    CLARITY_TRACKING_ID: process.env.NEXT_PUBLIC_CLARITY_ID,
+  },
 };
 
 export default nextConfig;
